98cea21a211e5297aee2e1da71c7641cac187635
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-15 20:20:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,20 @@
 class Triangle
- attr_accessor :side_length, :equilateral, :isosceles, :scalene
+ attr_accessor :kind
 
-  def initialize(side_length)
+def initialize(side1, side2, side3)
   side_length.each {|key, value| self.send(("#{key}="), value)}
   end
 
   def kind
+    self.triangle = (equilateral || isosceles || scalene)
+    if triangle.class != Triangle
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    else
+        .triangle = self
 
   end
 
