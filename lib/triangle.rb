4bf6827bbf8ba3d6ae7dfb79d8813f9761a583c3
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-15 22:42:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ def initialize(side1, side2, side3)
 
     def kind
 
-    if ((side1 + side2) <= side3) || ((side2 + side3) <= side1) || ((side1 + side3) <= side2) || (side1 * side2 * side3) == 0 || side1 == 0 && side2 == 0 && side3 == 0
+    if ((side1 + side2) <= side3) || ((side2 + side3) <= side1) || ((side1 + side3) <= side2) || (side1 * side2 * side3) == 0 || (side1 == 0 && side2 == 0 && side3 == 0)
         begin
           raise TriangleError
           puts error.message
