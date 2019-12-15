7a805111252866687df7b0565d848b38ed6d2a45
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-15 22:08:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,6 +2,9 @@ class Triangle
  attr_accessor :kind, :equilateral, :isosceles, :scalene, :side1, :side2, :side3
 
 def initialize(side1, side2, side3)
+  @side1 = side1
+  @side2 = side2
+  @side3 = side3
   #side_length.each {|key, value| self.send(("#{key}="), value)}
   end
 
