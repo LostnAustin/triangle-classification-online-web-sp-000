fdc11f281b4c1ece9db3459fd7017ba9cad5189d
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-15 20:07:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,13 @@
 class Triangle
  attr_accessor :equilateral, :isosceles, :scalene
 
-  def initialize(equilateral:, isosceles:, scalene:)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
+  def initialize(attributes)
+   attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
 
   class TriangleError < StandardError
 
   end
-  
+
 end
